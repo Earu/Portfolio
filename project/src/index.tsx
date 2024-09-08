ecd70dddfe57d8f89554c2f273cb703e1b18970c
Ryan <ryan.rbsr@gmail.com>
2024-09-08 20:38:24 +0200

    dont call changeLanguage if french browser is picked up

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -15,10 +15,10 @@ const pathChunks = window.location.pathname.substring(1).split('/');
 if (pathChunks[0] && pathChunks[0].toLowerCase() === "fr") {
 	i18n.changeLanguage("fr");
 } else {
-	i18n.changeLanguage("en");
-
-	if (navigator.language.split("-")[0] == "fr") {
+	if (navigator.language.split("-")[0] === "fr") {
 		window.location.href = "./fr"; // force switch to french page in case we have a french browser
+	} else {
+		i18n.changeLanguage("en");
 	}
 }
 
