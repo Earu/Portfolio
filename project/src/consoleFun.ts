68fffbe3ca79efb14ca864dd289f5baa4d9bc489
Ryan <ryan.rbsr@gmail.com>
2024-09-08 20:44:33 +0200

    fix repo url being wrong

--- project/src/consoleFun.ts
+++ project/src/consoleFun.ts
@@ -5,8 +5,8 @@ document.addEventListener("TranslationsLoaded", () => {
 	const { t } = i18next;
 
 	console.log(`%c${t("CONSOLE_FUN_HEADER")}`, "color: #FF0000; -webkit-text-stroke: 2px black; font-size: 72px; font-weight: bold;");
-	console.log(`%c${t("CONSOLE_FUN_TEXT", { GITHUB_URL: getPrivacyVariable("GITHUB_URL") })}\n`, "color: white; font-size: 17px;");
-	console.info(t("CONSOLE_FUN_INFO"));
+	console.log(`%c${t("CONSOLE_FUN_TEXT")}\n`, "color: white; font-size: 17px;");
+	console.info(t("CONSOLE_FUN_INFO", { GITHUB_URL: getPrivacyVariable("GITHUB_URL") }));
 });
 
 export {};
