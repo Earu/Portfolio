b77cb9de41354c84a081eb398f36271687015849
Ryan <ryan.rbsr@gmail.com>
2024-09-16 14:35:36 +0200

    apparently google search hates redirects

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -10,11 +10,11 @@ const pathChunks = window.location.pathname.substring(1).split('/');
 if (pathChunks[0] && pathChunks[0].toLowerCase() === "fr") {
 	i18n.changeLanguage("fr");
 } else {
-	if (navigator.language.split("-")[0] === "fr") {
-		window.location.href = "./fr"; // force switch to french page in case we have a french browser
-	} else {
+	//if (navigator.language.split("-")[0] === "fr") {
+	//	window.location.href = "./fr"; // force switch to french page in case we have a french browser
+	//} else {
 		i18n.changeLanguage("en");
-	}
+	//}
 }
 
 const container = document.getElementById('root') as HTMLElement;
